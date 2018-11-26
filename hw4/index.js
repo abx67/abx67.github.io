e35ca452e6655b72b40f590d28df2ada33eef86e
ya1995fatw@gmail.com <abx67>
2018-11-26 22:08:47 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -1,9 +1,9 @@
 
 const HOSTED_URLS = {
   model:
-      'model_js_lstm/model.json',
+      'model_js/model.json',
   metadata:
-      'model_js_lstm/metadata.json'
+      'model_js/metadata.json'
 };
 
 const examples = {
